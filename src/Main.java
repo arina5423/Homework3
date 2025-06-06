48488aac72ad2d7d81ac091278f68ea102c0858d
newuser <popovskaarisha123@gmail.com>
2025-06-06 20:19:20 +0300

    Задача 6

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        byte bank =120;
-        byte white =2;
-        byte brown =4;
-        int class1 = bank/(white+brown);
-        System.out.println("В школе,где "+ class1 + " классов,нужно " +class1*white + " банок белой краски и " + class1*brown + " банок коричненой краски");
-
+        short banana = 5*80;
+        short milk = 105*2;
+        short iceCream = 100*2;
+        short egg = 4*70;
+        int summa = banana+milk+iceCream+egg;
+        float summa1 = (banana+milk+iceCream+egg)/1000f;
+        System.out.println(summa + " грамм");
+        System.out.println(summa1 + " кг");
     }
 }
