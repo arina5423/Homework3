580d8bcb25e302de904af6b691a0af33018e6fff
newuser <popovskaarisha123@gmail.com>
2025-06-06 19:35:18 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        byte a = 1;
-        short b = 30000;
-        int c = 35000;
-        long d = 3000000000L;
-        float e = 1.5f;
-        double f = 1.567844673;
-        System.out.println("Значение переменной a с типом byte равно "+ a);
-        System.out.println("Значение переменной b с типом short равно "+ b);
-        System.out.println("Значение переменной c с типом int равно "+ c);
-        System.out.println("Значение переменной d с типом long равно "+ d);
-        System.out.println("Значение переменной e с типом float равно "+ e);
-        System.out.println("Значение переменной f с типом double равно "+ f);
+        short a = -159;
+        short b = 27897;
+        long c = 987678965549L;
+        short d = 569;
+        float e = 2.786f;
+        float f = 27.12f;
+        byte i = 67;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(i);
 
     }
 }
