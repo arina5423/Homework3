e55c7c2c2846cb115682ebab5b0073a59ceaad1c
newuser <popovskaarisha123@gmail.com>
2025-06-06 20:28:54 +0300

    Задача 7

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        short banana = 5*80;
-        short milk = 105*2;
-        short iceCream = 100*2;
-        short egg = 4*70;
-        int summa = banana+milk+iceCream+egg;
-        float summa1 = (banana+milk+iceCream+egg)/1000f;
-        System.out.println(summa + " грамм");
-        System.out.println(summa1 + " кг");
+        short needToReset = 7*1000;
+        short weight1 = 250;
+        short weight2 = 500;
+        System.out.println((needToReset/weight1) + " дней, если скидывать по " + weight1 + " грамм");
+        System.out.println((needToReset/weight2) + " дней, если скидывать по " + weight2 + " грамм");
+
+
+
     }
 }
