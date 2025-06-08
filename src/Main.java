ccd6d585bc15d3a6b61b778ec69ff119c5d99dda
newuser <popovskaarisha123@gmail.com>
2025-06-08 20:34:51 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // задача 1
+        /* задача 1
         byte a = 1;
         short b = 30000;
         int c = 35000;
@@ -15,6 +15,21 @@ public class Main {
         System.out.println("Значение переменной d с типом long равно "+ d);
         System.out.println("Значение переменной e с типом float равно "+ e);
         System.out.println("Значение переменной f с типом double равно "+ f);
+         задача 2 */
+        short a = -159;
+        short b = 27897;
+        long c = 987678965549L;
+        short d = 569;
+        float e = 2.786f;
+        float f = 27.12f;
+        byte i = 67;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(i);
 
 
     }
